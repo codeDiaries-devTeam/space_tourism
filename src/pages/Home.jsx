fb19c995225893bc52bab98b264d1d284dce9ee3
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-01-07 18:00:14 +0300

    feat: removed p tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,6 @@ const Home = () => {
         <div className=" text-white border border-white w-[450px] h-[382px] ">
           <h2>SO, YOU WANT TO TRAVEL TO</h2>
           <h1>SPACE</h1>
-          <p>
-            Let's face it: if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relaxe because we'll give you a truly out of this
-            world experience.
-          </p>
         </div>
         <div className=" flex justify-center items-center text-black border border-white w-[274px] h-[274px] bg-white rounded-full">
           <h1 className="">EXPLORE</h1>
