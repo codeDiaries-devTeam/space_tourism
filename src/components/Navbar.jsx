12a94672e03b6a44c4dbb847a77e7694d8d3a971
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-01-14 18:27:46 +0300

    feat: updated mobile btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,12 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="fixed md:mt-6 ">
+    <Disclosure as="nav" className="fixed md:mt-6 w-full">
       {({ open }) => (
-        <div className="min-w-full">
+        <div className="min-w-full ">
           <div className="w-full px-2 sm:px-6 lg:px-20">
             <div className="relative flex h-16 items-center justify-around">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 right-0 mr-8 flex items-center sm:hidden ">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Main</span>
@@ -32,7 +32,7 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between ">
+              <div className="flex flex-1 items-center justify-start ml-3 sm:items-stretch sm:justify-between ">
                 {/*flex-row sm:flex-row-reverse */}
                 <NavLink className="flex flex-shrink-0 items-center" to="/">
                   <img
@@ -47,7 +47,7 @@ export default function Navbar() {
                   />
                 </NavLink>
                 <div className=" ml-36 border-b border-gray-500 h-1 w-[600px] my-auto hidden md:block"></div>
-                <div className="hidden sm:ml-6 sm:block rgba(255, 255, 255, 0.04) backdrop-blur-2xl">
+                <div className="hidden sm:ml-6 sm:block backdrop-blur-2xl">
                   <div className="flex space-x-4 w-[650px] p-5  ">
                     {navigation.map((item) => (
                       <Link
@@ -67,12 +67,12 @@ export default function Navbar() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="sm:hidden backdrop-blur-2xl">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
-                  as="a" // link
+                  as={Link}
                   to={item.to}
                   className={classNames(
                     item.current
